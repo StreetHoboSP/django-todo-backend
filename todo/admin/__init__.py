479afed8b94b8c93399e05895213b40b65ef2aaa
StreetHoboSP <streethobosp@gmail.com>
2016-07-06 13:37:40 +0300

    Hide group model on admin page

--- todo/admin/__init__.py
+++ todo/admin/__init__.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from todo.admin.user import UserAdmin
 from todo.admin.todo import TodoAdmin
 from todo.models import User, Todo
 
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Todo, TodoAdmin)
+
+admin.site.unregister(Group)
